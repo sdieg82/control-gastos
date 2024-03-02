be6fc8eb2190187831b65145980e93f1de4ab473
Diego <sdieg82@gmail.com>
2024-03-02 14:17:10 -0500

    Modal Agregado, button open and close modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 
 function App() {
 
   const [presupuesto,setPresupuesto]=useState(0)
   const [isValidPresupuesto,setIsValidPresupuesto]=useState(false);
+  const [modal,setModal]=useState(false);
+  const [animarModal,setAnimarModal]=useState(false);
+
+  const handleNuevoGasto=()=>{
+    setModal(true)
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500);
+  }
   return (
     <div>
       <Header
@@ -13,6 +24,18 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img src={IconoNuevoGasto} 
+               alt="icono nuevo gasto" 
+               onClick={handleNuevoGasto}/>
+      </div>
+      )}
+      {modal &&  <Modal
+                  setModal={setModal}
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                 />}
     </div>
   )
 }
